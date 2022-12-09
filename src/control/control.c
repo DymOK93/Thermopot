e08c32ca225a703e5b224aa0efd23b6c5c6aa595
DymOK93 <dym93ok@yandex.ru>
2022-12-10 01:01:03 +0300

    Improved controller usability: using TmTemperatureMax as the default temperature point and TmModePid as the default mode

--- src/control/control.c
+++ src/control/control.c
@@ -15,6 +15,9 @@
 #define CTRL_TIMER_INTERRUPT_PRIORITY 3
 #define CTRL_BUTTON_DEBOUNCE_DELAY 100  // Milliseconds
 
+#define CTRL_DEFAULT_MODE TmModePid
+#define CTRL_DEFAULT_TEMPERATURE_POINT TmTemperatureMax
+
 typedef enum {
   CtrlStageError = -2,
   CtrlStageInitialized = -1,
@@ -59,7 +62,6 @@ typedef struct {
 static CtrlState g_ctrl_state = {
     .button_handler = {&CtrlpSetupMode, &CtrlpSetupTemperature},
     .stage = CtrlStageInitialized,
-    .settings = {.mode = TmModeRelay, .temperature_point = {0}},
     .scheduled_button = CtrlButtonCount,
     .temperature_step = Fp16Initialize(CTRL_TEMPERATURE_STEP, 0),
     .mode_label = {{"ler"},   // "rel"
@@ -77,6 +79,23 @@ static void CtrlpPrepareGpio(void) {
   SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOAEN);
   SET_BIT(GPIOA->PUPDR,
           GPIO_PUPDR_PUPDR0_1 | GPIO_PUPDR_PUPDR11_0 | GPIO_PUPDR_PUPDR12_0);
+}
+
+static void CtrlpSetupTimer(void) {
+  /*
+   * 1. Tick period is 1ms
+   * 2. One-pulse mode
+   * 3. User interrupt enable
+   */
+  SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM14EN);
+  TIM14->PSC = (uint16_t)(SystemCoreClock / 1000 - 1);  // (1)
+  TIM14->ARR = CTRL_BUTTON_DEBOUNCE_DELAY;
+
+  SET_BIT(TIM14->CR1, TIM_CR1_OPM);    // (2)
+  SET_BIT(TIM14->DIER, TIM_DIER_UIE);  // (3)
+
+  NVIC_SetPriority(TIM14_IRQn, CTRL_TIMER_INTERRUPT_PRIORITY);
+  NVIC_EnableIRQ(TIM14_IRQn);
 }
 
 static void CtrlpSetupKeyInterrupts(void) {
@@ -98,21 +117,9 @@ static void CtrlpSetupKeyInterrupts(void) {
   NVIC_EnableIRQ(EXTI4_15_IRQn);
 }
 
-static void CtrlpSetupTimer(void) {
-  /*
-   * 1. Tick period is 1ms
-   * 2. One-pulse mode
-   * 3. User interrupt enable
-   */
-  SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM14EN);
-  TIM14->PSC = (uint16_t)(SystemCoreClock / 1000 - 1);  // (1)
-  TIM14->ARR = CTRL_BUTTON_DEBOUNCE_DELAY;
-
-  SET_BIT(TIM14->CR1, TIM_CR1_OPM);    // (2)
-  SET_BIT(TIM14->DIER, TIM_DIER_UIE);  // (3)
-
-  NVIC_SetPriority(TIM14_IRQn, CTRL_TIMER_INTERRUPT_PRIORITY);
-  NVIC_EnableIRQ(TIM14_IRQn);
+static void CtrlpUpdateDefaults(void) {
+  g_ctrl_state.settings.mode = CTRL_DEFAULT_MODE;
+  g_ctrl_state.settings.temperature_point = CTRL_DEFAULT_TEMPERATURE_POINT;
 }
 
 static void CtrlpSetupMode(CtrlButton button) {
@@ -135,13 +142,13 @@ static void CtrlpSetupTemperature(CtrlButton button) {
 
   if (button == CtrlButtonUp) {
     Fp16Add(temperature_point, settings->temperature_point,
-          g_ctrl_state.temperature_step);
+            g_ctrl_state.temperature_step);
     if (Fp16LessEqual(temperature_point, TmTemperatureMax)) {
       settings->temperature_point = temperature_point;
     }
   } else if (button == CtrlButtonDown) {
     Fp16Sub(temperature_point, settings->temperature_point,
-          g_ctrl_state.temperature_step);
+            g_ctrl_state.temperature_step);
     if (Fp16GreaterEqual(temperature_point, TmTemperatureMin)) {
       settings->temperature_point = temperature_point;
     }
@@ -215,6 +222,7 @@ TpStatus CtrlInitialize(void) {
   CtrlpPrepareGpio();
   CtrlpSetupTimer();
   CtrlpSetupKeyInterrupts();
+  CtrlpUpdateDefaults();
   return TpSuccess;
 }
 
